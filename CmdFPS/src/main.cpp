c3f17fdcbd58f0cff547a1be2183a624623ab158
Philip Seitz <philip.seitz@outlook.de>
2022-11-01 12:54:45 +0100

    screen buffer and output added

--- CmdFPS/src/main.cpp
+++ CmdFPS/src/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
-	cout << "McGregor" << endl;
+	// Screen Buffer initialization ===================================
+	int screen_w = 120;
+	int screen_h = 40;
+	wchar_t* sbuf = new wchar_t[screen_w * screen_h];
 
+	HANDLE sbuf_h = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	DWORD dwBytesWritten = 0;
+	SetConsoleActiveScreenBuffer(sbuf_h);
+
+	for (int x = 0; x < screen_w; x++)
+	{
+		for (int y = 0; y < screen_h; y++)
+		{
+			sbuf[y * screen_w + x] = L' ';
+
+		}
+	}
+
+	// Game Loop ====================================================== 
+	while (1)
+	{
+		WriteConsoleOutputCharacter(sbuf_h, sbuf, screen_w * screen_h, { 0,0 }, &dwBytesWritten);
+	}
 
 	return 0;
-
 }
